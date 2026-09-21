cd42455bcab4876ffcfdb22277b02a2dfa68e16b
genwilliam <178803510+genwilliam@users.noreply.github.com>
2026-09-21 17:24:34 +0800

    fix(cask): satisfy Homebrew style and audit

--- Casks/surge@6.6.0.rb
+++ Casks/surge@6.6.0.rb
@@ -2,15 +2,13 @@ cask "surge@6.6.0" do
   version "6.6.0,11270"
   sha256 "b88f8534354f3774e35b0280b5b6308c9f6c3ff5da5538efd42e647292063e21"
 
-  url "https://dl.nssurge.com/mac/v6/Surge-6.6.0-11270-68599760a9dfa8ea625dd4ce491e534e.zip"
-
+  url "https://dl.nssurge.com/mac/v6/Surge-#{version.csv.first}-11270-68599760a9dfa8ea625dd4ce491e534e.zip"
   name "Surge"
   desc "Advanced network toolbox"
   homepage "https://nssurge.com/"
 
-  depends_on macos: :monterey
-
   auto_updates false
+  depends_on macos: :monterey
 
   app "Surge.app"
 end
